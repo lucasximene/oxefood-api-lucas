45ff00b8538088d80894e1c900b8837297c610ee
= <=>
2024-08-15 16:41:03 -0300

    novos arquivos

--- src/main/java/br/com/ifpe/oxefood/config/SecurityConfiguration.java
+++ src/main/java/br/com/ifpe/oxefood/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.ifpe.oxefood.config;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import br.com.ifpe.oxefood.modelo.acesso.Usuario;
 import br.com.ifpe.oxefood.modelo.seguranca.JwtAuthenticationFilter;
 
 @Configuration
@@ -25,7 +27,6 @@ public class SecurityConfiguration {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     public SecurityConfiguration(JwtAuthenticationFilter jwtAuthenticationFilter, AuthenticationProvider authenticationProvider) {
-
         this.authenticationProvider = authenticationProvider;
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
@@ -33,45 +34,39 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-
         http
-            .csrf().disable() // Desativa a proteção CSRF (opcional, para facilitar o desenvolvimento)
-            .authorizeRequests()
-                .anyRequest().permitAll() // Permite todas as requisições
-            .and()
-            .httpBasic(); // Opcional, você pode configurar para usar basic auth ou remover
+            .csrf(c -> c.disable())
+            .authorizeHttpRequests(authorize -> authorize
 
-
-
-        /*http.csrf()
-                .disable()
-                .authorizeHttpRequests()               
                 .requestMatchers(HttpMethod.POST, "/api/cliente").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/auth").permitAll()
 
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
+                .requestMatchers(HttpMethod.GET, "/api-docs/*").permitAll()
+                .requestMatchers(HttpMethod.GET, "/swagger-ui/*").permitAll()
+
+                .anyRequest().authenticated()
+
+            )
+            .sessionManagement((session) -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-*/
+            )            
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
+    public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:8082"));
-        configuration.setAllowedMethods(List.of("GET", "POST"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-
+        configuration.setAllowCredentials(true);
+    
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration("/**", configuration);    
         return source;
     }
 }
